1442cf64d668b65206cc998ccfef65d3f4548756
cazukt <126223060+cazukt@users.noreply.github.com>
2023-04-04 11:38:01 +0900

    1 file modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,7 +11,12 @@ import { Product } from '../shared/product.model';
 export class ProductListComponent {
 
   // products = products;
-  products:Product = products;
+  //products:Product = products;
+  products:Product = {
+    name:"",
+    price:0,
+    description:"",
+  }
 
   // number:number=5;
   // stname:string="";
